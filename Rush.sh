648443470e17afd52d300f2c6ed086a6b291eda1
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-18 09:07:01 +0530

    Add files via upload

--- Rush.sh
+++ Rush.sh
@@ -1,2 +1,2 @@
 apt -qq update && apt -qq install -y git wget pv jq wget python3-dev ffmpeg mediainfo curl 
-sh -c "$(curl -f sSl https://raw.githubusercontent.com/rooted-cyber/okteto-compressor/main/dcompre.sh)"
+sh -c "$(curl -f sSl https://raw.githubusercontent.com/rooted-cyber/okteto-compressor/main/dcompres.sh)"
