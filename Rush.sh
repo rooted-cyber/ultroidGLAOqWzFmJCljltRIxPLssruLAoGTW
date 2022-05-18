06002f2e3399dfbd9b1b41099adfb3755b00a8b1
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-18 09:43:13 +0530

    Add files via upload

--- Rush.sh
+++ Rush.sh
@@ -1,5 +1,14 @@
+ab() {
+aiohttp
+python-decouple 
+pyaesni 
+https://github.com/1Danish-00/Telethon/archive/master.zip
+html_telegraph_poster requests
+telethon
+}
 apt -qq update && apt -qq install -y git wget pv jq wget python3-dev ffmpeg mediainfo curl 
 pip install --upgrade pip
 pip install requests
 pip3 install requests
+pip3 install ab
 sh -c "$(curl -fsSl https://raw.githubusercontent.com/rooted-cyber/okteto-compressor/main/dcompres.sh)"
